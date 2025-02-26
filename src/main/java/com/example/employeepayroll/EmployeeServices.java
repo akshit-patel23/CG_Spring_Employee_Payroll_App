ecd4c58b0c2f0e7e3624ab9b3f5d8342a872c48e
Akshit Patel <akshitpatel.g@gmail.com>
2025-02-26 14:40:07 +0530

    [Akshit] Add . Services Layer in Employee Payroll App

--- src/main/java/com/example/employeepayroll/EmployeeServices.java
+++ src/main/java/com/example/employeepayroll/EmployeeServices.java
@@ -12,33 +12,30 @@ import java.util.Optional;
 public class EmployeeServices {
     private List<Employee> list=new ArrayList<>();
 
+@Autowired
+private EmployeeRepository employeeRepository;
 
-    public void add(Employee employee){
+    public Employee add(Employee employee){
         list.add(employee);
+        return employeeRepository.save(employee);
     }
 
     public boolean delete(Long id){
-        for(Employee emp:list){
-            if(emp.getId()==id){
-                list.remove(emp);
-                return true;
-            }
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
         }
         return false;
     }
     public List<Employee> fetchAll(){
-        return list;
+        return employeeRepository.findAll();
     }
 
-    public boolean update(long id ,Employee updateEmployee) {
-        for(Employee emp:list){
-            if(emp.getId()==id){
-                list.remove(emp);
-                list.add(updateEmployee);
-                return true;
-            }
-        }
-        return false;
+    public Employee update(long id ,Employee updateEmployee) {
+        return employeeRepository.findById(id).map(employee -> {employee.setName(updateEmployee.getName());
+            employee.setSalary(updateEmployee.getSalary());
+            return employeeRepository.save(employee);
+        }).orElse(null);
     }
     public Employee check(long id){
 
