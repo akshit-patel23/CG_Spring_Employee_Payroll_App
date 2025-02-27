d5b215e2e1f5c002a0da6b35ff43a75d2473aa01
Akshit Patel <akshitpatel.g@gmail.com>
2025-02-27 15:45:54 +0530

    [Akshit] Add . Code for adding remaining properties to the payroll DTO

--- src/main/java/com/example/employeepayroll/Employee.java
+++ src/main/java/com/example/employeepayroll/Employee.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "employees")
@@ -19,4 +22,11 @@ public class Employee {
     @Pattern(regexp = "^[A-Z][a-zA-z ]{2,}$",message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
     private double salary;
+
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    
+    private List<String> department;
 }
